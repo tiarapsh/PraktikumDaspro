e3526cbb67624d4ce1fd25813786f51532eb3e5c
tiara <haiiiinirara@gmail.com>
2025-10-09 11:23:37 +0700

    changen name

--- jobsheet5/ifCetakKRS22.java
+++ jobsheet5/ifCetakKRS22.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ifCetakKRSNoPresensi {
+public class ifCetakKRS22 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("--- Print KRS SIAKAD ---");
