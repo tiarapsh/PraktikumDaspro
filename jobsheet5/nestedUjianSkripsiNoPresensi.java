5b96d24cada3f93542955bf01f2c01e9ef26d70e
tiara <haiiiinirara@gmail.com>
2025-10-09 14:24:04 +0700

    fix attemp 3

--- jobsheet5/nestedUjianSkripsiNoPresensi.java
+++ jobsheet5/nestedUjianSkripsiNoPresensi.java
@@ -7,23 +7,21 @@ public class nestedUjianSkripsiNoPresensi {
         System.out.println("Has the student been cleared of compensation? (Y/T or Yes/No):");
         String compensationStatus = scan.nextLine().trim();
 
-        System.out.println("Enter the number of guidance logs with Supervisor 1:");
-        int supervisor1 = scan.nextInt();
-
-        System.out.println("Enter the number of guidance logs with Supervisor 2:");
-        int supervisor2 = scan.nextInt();
-
-        if(compensationStatus.equalsIgnoreCase("Yes") || compensationStatus.equalsIgnoreCase("Y") ){
-            if (supervisor1 >= 8 && supervisor2 >= 4){
+        if (compensationStatus.equalsIgnoreCase("Yes") || compensationStatus.equalsIgnoreCase("Y")) {
+            System.out.println("Enter the number of guidance logs with Supervisor 1:");
+            int supervisor1 = scan.nextInt();
+            System.out.println("Enter the number of guidance logs with Supervisor 2:");
+            int supervisor2 = scan.nextInt();
+            if (supervisor1 >= 8 && supervisor2 >= 4) {
                 message = "All requirements are met. The student may register for the thesis exam.";
-            }else if ( supervisor1 < 8 && supervisor2 < 4){
+            } else if (supervisor1 < 8 && supervisor2 < 4) {
                 message = "Failed: Guidance logs with Supervisor 1 are fewer than 8 AND Supervisor 2 fewer then 4";
-            }else if (supervisor1 < 8 && supervisor2 >= 4){
+            } else if (supervisor1 < 8 && supervisor2 >= 4) {
                 message = "Failed: Guidance logs with Supervisor 1 are fewer than 8";
-            }else if (supervisor1 >= 8 && supervisor2 < 4){
+            } else if (supervisor1 >= 8 && supervisor2 < 4) {
                 message = "Failed: Guidance logs with Supervisor 2 are fewer than 4";
-            }  
-        }else {
+            }
+        } else {
             message = "Failed: The student has not been cleared of compensation.";
         }
 
