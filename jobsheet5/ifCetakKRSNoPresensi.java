2f3b335b51f57ac7480312a36105a98fdad18d98
tiara <haiiiinirara@gmail.com>
2025-10-09 11:07:45 +0700

    Attemp 1

---
+++ jobsheet5/ifCetakKRSNoPresensi.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ifCetakKRSNoPresensi {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("--- Print KRS SIAKAD ---");
+        System.out.println("Have the tuition fees been paid in full (true/false):");
+        boolean uktLunas = scan.nextBoolean();
+
+        if(uktLunas){
+            System.out.println("UKT payment has been verified");
+            System.out.println("KRS now can be printed and you can ask the academic advisor to sign it.");
+        }
+    }
+}
