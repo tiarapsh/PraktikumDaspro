b972c6e9eb9a84c94ff961275315019da7223a7a
tiara <haiiiinirara@gmail.com>
2025-10-09 14:35:42 +0700

    fix task 1

--- jobsheet5/ifCetakKRS22.java
+++ jobsheet5/ifCetakKRS22.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class ifCetakKRS22 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -6,19 +7,20 @@ public class ifCetakKRS22 {
         System.out.println("Have the tuition fees been paid in full (true/false):");
         boolean uktLunas = scan.nextBoolean();
 
-        if(uktLunas){
+        if (uktLunas) {
             System.out.println("UKT payment has been verified");
             System.out.println("KRS now can be printed and you can ask the academic advisor to sign it.");
-        }else {
+        } else {
             System.out.println("Registration rejected. Please pay UKT first.");
         }
 
+        System.out.println("--- Using Ternary Operator --- ");
         String message = uktLunas
                 ? "UKT payment has been verified.\nKRS now can be printed and you can ask the academic advisor to sign it."
                 : "Registration rejected. Please pay UKT first.";
         System.out.println(message);
-    
-       {
+
+        {
 
         }
     }
